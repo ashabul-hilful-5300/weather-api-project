33e52675f4e14fc8a3cef4f9d37acbb04ee4062e
Hilful <hilful25@gmail.com>
2022-02-17 00:50:25 +0600

    Making GET Request with the node js https module version updated with a different url

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const https = require ("https");
 
 const app =  express();
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=23.777176&lon=90.399452&appid=c842a18c6341c30721ca65fb7e1014c0";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Dhaka&appid=c842a18c6341c30721ca65fb7e1014c0&units=metric";
 app.get("/",(req , res)=>{
     https.get(url , (response)=>{
         console.log(response);
