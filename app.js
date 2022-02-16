90eee9a49e272e183deb6ad20b5815347e6ae01e
Hilful <hilful25@gmail.com>
2022-02-17 00:34:02 +0600

    Making GET Request with the node js https module

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
+const https = require ("https");
 
 const app =  express();
-
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=23.777176&lon=90.399452&appid=c842a18c6341c30721ca65fb7e1014c0";
 app.get("/",(req , res)=>{
+    https.get(url , (response)=>{
+        console.log(response);
+
+    })    
+
+
         res.send("I am working perfectly,yay!!")
 })
 
