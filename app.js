e3ed3ec56181a3b3eb10c19423964ca129628767
Ashabul Elaph Hilful <hilful25@gmail.com>
2023-12-31 19:30:06 +0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const https = require ("https");
 
 const app =  express();
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Dhaka&appid=c842a18c6341c30721ca65fb7e1014c0&units=metric";
+const url = "https://api.openweathermap.org/data/2.5/weather?";
 app.get("/",(req , res)=>{
     https.get(url , (response)=>{
         console.log(response);
